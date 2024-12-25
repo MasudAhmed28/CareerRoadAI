e2245c72a28911f5b36e1e08bf9dd35eb7b6fc9c
MasudAhmed28 <72435078+MasudAhmed28@users.noreply.github.com>
2024-12-25 16:02:32 +0530

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebase";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import SignInwithGoogle from "./SignInwithGoogle";
 import LoadSpinner from "./LoadSpinner";
 
