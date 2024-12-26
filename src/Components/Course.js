9e1cfa612225d17ef56553fe8882f3ebc773e5fe
MasudAhmed28 <72435078+MasudAhmed28@users.noreply.github.com>
2024-12-26 18:22:27 +0530

    Update Course.js

--- src/Components/Course.js
+++ src/Components/Course.js
@@ -9,7 +9,7 @@ import { useParams } from "react-router-dom";
 
 const Course = () => {
   const [courses, setCourses] = useState([]);
-  const { loading, roadmapData } = useContext(DataContext);
+  const {roadmapData } = useContext(DataContext);
   const [topic, setTopic] = useState("");
   const [loading, setLoading] = useState(false);
   const { topicId } = useParams();
